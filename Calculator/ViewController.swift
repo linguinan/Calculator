606ce944896c11e174dcb3e0b2eb9fe757aebd1a
munan008 <linguinan@126.com>
2018-08-14 15:03:39 +0800

    Initial commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by FoxZhou on 2018/8/10.
+//  Copyright © 2018年 itools. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var txtLabel: UILabel!
+    
+    var userHasInit: Bool = false
+    
+    var brain = CalculatorBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func onNumClick(_ sender: UIButton) {
+        if(userHasInit)
+        {
+            txtLabel.text = txtLabel.text! + sender.currentTitle!
+        }else{
+            txtLabel.text = sender.currentTitle!
+            userHasInit = true
+        }
+    }
+    
+    @IBAction func operate(_ sender: UIButton) {
+        if userHasInit {
+            enter()
+        }
+        if let operation = sender.currentTitle {
+            if let result = brain.performOperation(symbol: operation) {
+                displayValue = result
+            } else {
+                displayValue = 0
+            }
+        }
+    }
+    
+    @IBAction func enter() {
+        userHasInit = false
+        if let result = brain.pushOperand(operand: displayValue) {
+            displayValue = result
+        } else {
+            displayValue = 0
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NumberFormatter().number(from: txtLabel.text!)!.doubleValue
+        }
+        set {
+            txtLabel.text = "\(newValue)"
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
